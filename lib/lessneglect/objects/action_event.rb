43a681e5f31d910505555a75b0ed933536deecfa
Christopher Gooley <c@thegooley.com>
2012-11-07 23:36:32 -0800

    added external_identifier and extras

--- lib/lessneglect/objects/action_event.rb
+++ lib/lessneglect/objects/action_event.rb
@@ -1,7 +1,7 @@
 class LessNeglectApi
 	class ActionEvent < Event
 
-    attr_accessor :note, :links
+    attr_accessor :note, :links, :external_identifier, :extras
 
     def add_link(name, href)
       self.links ||= []
@@ -12,6 +12,8 @@ class LessNeglectApi
       super.merge({
         :klass => "actionevent",
         :note => self.note,
+        :external_identifier => self.external_identifier,
+        :extras => self.extras,
         :links => self.links.nil? ? nil : self.links.as_json
       }).as_json(options)
     end
