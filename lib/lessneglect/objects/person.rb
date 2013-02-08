1b3a8cfebc38b24f6173dcfc59ad0d86008f8307
Zach Millman <zach.millman@gmail.com>
2013-02-07 18:03:34 -0800

    Remove unused :id attribute from Person

--- lib/lessneglect/objects/person.rb
+++ lib/lessneglect/objects/person.rb
@@ -1,10 +1,9 @@
 class LessNeglect::Person < LessNeglect::ApiObject
     
-  attr_accessor :id, :name, :email, :external_identifier, :properties
+  attr_accessor :name, :email, :external_identifier, :properties
     
   def as_json(options={})
     {
-      :id                  => self.id,
       :name                => self.name,
       :email               => self.email,
       :external_identifier => self.external_identifier,
