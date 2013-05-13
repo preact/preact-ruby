b216da28db3d4f39437e1413309f1cbfad1a1727
Christopher Gooley <c@thegooley.com>
2013-05-13 15:45:54 -0700

    fixed created_at accessor on person to use new simplified location

--- lib/preact/objects/person.rb
+++ lib/preact/objects/person.rb
@@ -1,12 +1,13 @@
 class Preact::Person < Preact::ApiObject
     
-  attr_accessor :name, :email, :external_identifier, :properties, :uid
+  attr_accessor :name, :email, :external_identifier, :properties, :uid, :created_at
     
   def as_json(options={})
     {
       :name                => self.name,
       :email               => self.email,
       :uid                 => self.uid || self.external_identifier,
+      :created_at          => self.created_at.to_i,
       :properties          => self.properties
     }
   end
